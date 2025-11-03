836dfa75ac472a113a260ea7c1d02b4c01231339
Diintech7 <diintechteam7@gmail.com>
2025-11-03 21:47:47 +0530

    lets see

--- config/kafka.js
+++ config/kafka.js
@@ -1,6 +1,21 @@
 const { Kafka } = require('kafkajs');
-const brokers = process.env.KAFKA_BROKER.split(',');
-const kafka = new Kafka({ brokers });
+const { awsIamAuthenticator } = require('@jm18457/kafkajs-msk-iam-authentication');
+
+const brokers = (process.env.KAFKA_BROKER || '')
+  .split(',')
+  .map(b => b.trim())
+  .filter(Boolean);
+
+const kafka = new Kafka({
+  clientId: process.env.KAFKA_CLIENT_ID || 'aitota-backend',
+  brokers,
+  ssl: true,
+  sasl: {
+    mechanism: 'aws',
+    authenticationProvider: awsIamAuthenticator,
+  },
+});
+
 const producer = kafka.producer();
-producer.connect();
+producer.connect().catch(() => {});
 module.exports = producer;
